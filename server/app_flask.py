0a16e7bb747f9123e62756967f9ad7d85d18286d
jono <on@oj.io>
2013-10-20 11:33:05 -0700

    added /relays

--- server/app_flask.py
+++ server/app_flask.py
@@ -1,11 +1,23 @@
 #!/usr/bin/python
 from flask import Flask
+from flask import jsonify
+from sendHub import calculateRoutes
+from sendHub import backend_resources
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return "sendHub"
+
+
+@app.route('/relays/routes', methods=['GET'])
+def calculate_routes():
+    pass
+
+@app.route('/relays', methods=['GET'])
+def get_relays():
+    return jsonify(backend_resources)
 
 if __name__ == '__main__':
     app.run(debug=True)
